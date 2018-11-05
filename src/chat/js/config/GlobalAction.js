66c4038976dc6bfca20db53f527af13973827f2d
caimoxuan <391850846@qq.com>
2018-11-05 23:26:24 +0800

    add logout

--- src/chat/js/config/GlobalAction.js
+++ src/chat/js/config/GlobalAction.js
@@ -3,7 +3,7 @@ import {message} from 'antd';
 import {createBrowserHistory as createHistory} from 'history';
 
 const history = createHistory();
-
+/** 登录返回用户基本信息 */
 export const LoginAction = function(data) {
     let api = '/accountLogin';
     window.Axios.post(api, data).then(function(res){
@@ -18,3 +18,14 @@ export const LoginAction = function(data) {
         message.error('System error, please try later!');
     })
 };
+
+export const LogOut = function(){
+    let api = '/logout';
+    window.Axios.post(api).then(function(res){
+        if(res.data.success){
+            history.push('/login');
+        }
+    }).catch(function(error){
+        message.error('logout fail, please try later or close window directly');
+    })
+}
